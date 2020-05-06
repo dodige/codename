8a8aca09d05138bf0c7c19f5fbbacae9bdb0512f
donaldodige <dodige@gmail.com>
2020-05-06 19:53:13 -0400

    utf8
    utf8

--- server/game.js
+++ server/game.js
@@ -6,7 +6,7 @@ let readline = require('readline')
 let basewords = []
 var filename = './server/words.txt'
 readline.createInterface({
-    input: fs.createReadStream(filename, {encoding: 'binary'}),
+    input: fs.createReadStream(filename, {encoding: 'utf-8'}),
     terminal: false
 }).on('line', (line) => {basewords.push(line)})
 
