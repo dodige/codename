13eaa6f9392430ccb97ec0a1a514eaa071fd51f3
donaldodige <dodige@gmail.com>
2020-05-06 22:08:58 -0400

    Update game.js

--- server/game.js
+++ server/game.js
@@ -6,7 +6,7 @@ let readline = require('readline')
 let basewords = []
 var filename = './server/words.txt'
 readline.createInterface({
-    input: fs.createReadStream(filename, {encoding: 'null'}),
+    input: fs.createReadStream(filename),
     terminal: false
 }).on('line', (line) => {basewords.push(line)})
 
