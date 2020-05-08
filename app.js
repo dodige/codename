0f4c173fd5eda5423ae90ac047ba63dce6b0d5b5
donaldodige <dodige@gmail.com>
2020-05-08 02:02:41 -0400

    ok

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ function listen(){
   console.log('Codenames Server Started at http://' + host + ':' + port);
 }
 
-// Force SSL
+Force SSL
 app.use((req, res, next) => {
   if (req.header('x-forwarded-proto') !== 'https') {
     res.redirect(`https://${req.header('host')}${req.url}`)
   } else {
     next();
-  }
+ }
 });
 
 // Files for client
